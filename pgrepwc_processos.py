e236f780ab752fe5a6f5ba1a484d29c689ef54d7
ilihab148 <48345664+ilihab148@users.noreply.github.com>
2022-11-02 22:26:17 +0000

    Update pgrepwc_processos.py

--- pgrepwc_processos.py
+++ pgrepwc_processos.py
@@ -3,42 +3,46 @@
 ### Aluno 2: Eduardo Viana (fc58776)
 ### Aluno 3: Iliyan Habibo (fc58626)
 
+import multiprocessing 
 import sys
 
 
 def pesquisa(ficheiro,palavra):
     """Recebe: um ficheiro e uma palavra. 
     Procura a palavra nesse ficheiro, imprime as linhas do ficheiro em que a palavra aparece.
-    Retorna dicionario com numero de oco
-        """
+    Returns: dicionario com numero de ocorrencias da palavra no ficheiro e o numero de linhas que contem a palavra
+    """
     with open (ficheiro,"r") as f:
         counter_ocorrencias = 0
         counter_linhas = 0
-        linhas_out = [] #indices das linhas em que aparece a palavra
-        for linha in f.readlines(): #le linhas do ficheiro
+        lista_linhas = [] #indices das linhas em que aparece a palavra
+        for linha in f: #le linhas do ficheiro
             for palavra_pesquisa in linha.split(): #le cada palavra da linha corrente
                 if palavra_pesquisa == palavra: 
                     counter_ocorrencias += 1
-                    if counter_linhas not in linhas_out:    #para evitar que tenhamos duas linhas repetidas
-                        print(counter_linhas)
-                        linhas_out.append(counter_linhas)
+                    if counter_linhas not in lista_linhas:    #para evitar que tenhamos duas linhas repetidas
+                        print (linha) #print das linhas em que a palavra aparece
+                        lista_linhas.append(counter_linhas)
             counter_linhas +=1
     
-    return (counter_ocorrencias,linhas_out)
-
-
+    return {
+        "n_ocorrencias" : counter_ocorrencias,
+        "n_linhas" : len(lista_linhas),
+    }
 
 def main(args):
     print('Programa: pgrepwc_processos.py')
     print('Argumentos: ', args)
     ficheiros = args[-1].split(",")
     word = args[-2]
-    lista_tuplos = [] #contem tuplos cuja pos 0 e o n_ocorrencias da palavra no ficheiro e pos 1 e uma lista de linhas em que aparece a palavra
-    #ocorrencias_palavra = [ocorrencias_ficheiro1,ocorrencias_ficheiro2,etc]
-    for ficheiro in ficheiros:
-       lista_tuplos.append(pesquisa (ficheiro,word))
-    print (lista_tuplos)
-
+    lista_dicts = [] #lista que contem dicionarios com numero de ocorrencias da palavra num certo ficheiro e numero de linhas em que a palavra ocorre
+    if "-p" not in args:
+        for ficheiro in ficheiros:
+            lista_dicts.append(pesquisa (ficheiro,word))
+    if "-p" in args:
+        for i in range(len(ficheiros)): #criamos processos. para cada ficheiro criamos um processo
+            process = multiprocessing.Process(target = pesquisa, args = (ficheiros[i],word) )
+            print (process)
 
 
 
