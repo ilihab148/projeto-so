610f6431ec75d343df0ccfd5e6a8deca2c6d1170
ilihab148 <48345664+ilihab148@users.noreply.github.com>
2022-11-06 23:20:40 +0000

    Update pgrepwc_processos.py

--- pgrepwc_processos.py
+++ pgrepwc_processos.py
@@ -7,25 +7,29 @@
 import multiprocessing
 import sys
 
-#tenho que declarar word e ficheiros aqui p poder usar nos shared arrays
+#tenho que declarar word e ficheiros aqui para poder usar nos shared arrays
 for i in range(len(sys.argv[1:])):
     if "-p" in sys.argv[1:]:
-        nProcessos=int(sys.argv[1:][int(sys.argv[1:].index("-p")) + 1]) #sabemos que o num processos vem a seguir ao -p (na lista args), entao vamos busca-lo usando args[indice a seguir a -p]
-        #aqui porque ja tenho um numero e o argumento antes da palavra nao leva um "-" tenho que mudar os indices das pos da word e ficheiros
+        nProcessos=int(sys.argv[1:][int(sys.argv[1:].index("-p")) + 1]) #sabemos que o num processos vem a seguir ao -p (na lista args),
+        #entao vamos busca-lo usando args[indice a seguir a -p]
+        #aqui porque ja tenho um numero e o argumento antes da palavra nao leva um "-" 
+        # tenho que mudar os indices das pos da word e ficheiros
         word = sys.argv[1:][sys.argv[1:].index(str(nProcessos)) + 1]
         ficheiros = sys.argv[1:][sys.argv[1:].index(word) +1 :]
     else:
         if sys.argv[1:][i][0] != "-":
             word = sys.argv[1:] [i]
             ficheiros = sys.argv[1:] [i+1:]
-            break #tenho que dar o break senao ele continua o for e vai atualizando o word e os ficheiros para a string que aparece a seguir
+            break #tenho que dar o break senao ele continua o for e vai atualizando o word e os ficheiros para a string que 
+            #aparece a seguir
 
-   
+
 shared_array_nocorrencias = multiprocessing.Array("i",len(ficheiros)) #lista c ocorrencias da palavra.
 #continuacao da linha acima: indice 0 = ocorrencias da palavra no ficheiro na posicao 0 (em ficheiros)
 shared_array_nlinhas = multiprocessing.Array("i",len(ficheiros)) #lista c n_linhas em que a palavra aparece no ficheiro
 
-def pesquisa(lista_indice_ficheiros,lista_ficheiros,palavra,lista_queues,array_ocorrencias,array_linhas): #tenho q por os arrays aqui pq os filhos n conseguem ler os arrays criados acima
+def pesquisa(lista_indice_ficheiros,lista_ficheiros,palavra,lista_queues,array_ocorrencias,array_linhas): #tenho q por os arrays aqui
+    # pq os filhos n conseguem ler os arrays criados acima
     for i in range(len(lista_ficheiros)):
         with open (lista_ficheiros[i],"r") as f:
             counter_ocorrencias = 0
@@ -49,11 +53,12 @@ def main(args):
     if "-p" not in args:
         for i in range(len(ficheiros)):
             q = multiprocessing.Queue()
-            pesquisa ([i],[ficheiros[i]],word,[q],shared_array_nocorrencias,shared_array_nlinhas)
+            lista_queues =[q]
+            pesquisa ([i],[ficheiros[i]],word,lista_queues,shared_array_nocorrencias,shared_array_nlinhas)
             #print do nome do ficheiro
             print (f'\nNOME DO FICHEIRO: {ficheiros[i]}\n')
             #imprime linhas que estao na queue ate a queue estar vazia
-            while q.empty() == False:
+            while lista_queues[0].empty() == False:
                 print (q.get())
             
 
